d8d9933f8852b966b6f50ec6bbc52ec508e43b1a
Wolfgang Steiner <wolfgang.steiner@gmail.com>
2016-07-04 17:49:29 +0200

    json working!

--- app/src/main/java/com/example/android/booklisting/MainActivity.java
+++ app/src/main/java/com/example/android/booklisting/MainActivity.java
@@ -12,10 +12,14 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
-import java.util.ArrayList;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-public class MainActivity extends AppCompatActivity implements OnRestTaskCompleted
-{
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity implements OnRestTaskCompleted {
     ArrayList<Book> mBookList;
     BookAdapter mBookAdapter;
     RestTask mRestTask;
@@ -32,38 +36,31 @@ public class MainActivity extends AppCompatActivity implements OnRestTaskComplet
         listView.setAdapter(mBookAdapter);
     }
 
-    public void onStartSearch(View aView)
-    {
+    public void onStartSearch(View aView) {
         ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
 
-        if (networkInfo != null && networkInfo.isConnected())
-        {
+        if (networkInfo != null && networkInfo.isConnected()) {
 
             EditText searchField = (EditText) findViewById(R.id.search_field);
             String searchString = searchField.getText().toString();
 
-            if (!searchString.trim().isEmpty())
-            {
+            if (!searchString.trim().isEmpty()) {
                 Uri myUri = buildUri(searchString);
                 new RestTask(this).execute(myUri);
             }
-        }
-        else
-        {
+        } else {
             Toast.makeText(this, R.string.network_not_reachable, Toast.LENGTH_SHORT).show();
         }
     }
 
-    public void onRestTaskCompleted(String aResultString)
-    {
+    public void onRestTaskCompleted(String aResultString) {
         mBookList.clear();
-        mBookList.add(new Book("20000 miles under the sea", "Jules Verne"));
+        parseJson(aResultString);
         mBookAdapter.notifyDataSetChanged();
     }
 
-    private Uri.Builder buildUriBuilder()
-    {
+    private Uri.Builder buildUriBuilder() {
         Uri.Builder builder = new Uri.Builder();
         builder.scheme("https")
                 .authority("www.googleapis.com")
@@ -73,13 +70,56 @@ public class MainActivity extends AppCompatActivity implements OnRestTaskComplet
         return builder;
     }
 
-    private Uri buildUri(String aSearchString)
-    {
+    private Uri buildUri(String aSearchString) {
         Uri.Builder builder = buildUriBuilder();
         builder.appendPath("volumes");
         builder.appendQueryParameter("q", aSearchString);
         builder.appendQueryParameter("key", BuildConfig.GOOGLE_BOOKS_API_KEY);
 
         return builder.build();
+    }
+
+    private void parseJson(String jsonString) {
+        try
+        {
+            JSONObject jsonObj = new JSONObject(jsonString);
+            JSONArray volumes = jsonObj.getJSONArray("items");
+
+            for (int i = 0; i < volumes.length(); i++) 
+            {
+                JSONObject volume = volumes.getJSONObject(i);
+                JSONObject volumeInfo = volume.getJSONObject("volumeInfo");
+                JSONArray authors = volumeInfo.getJSONArray("authors");
+                String title = volumeInfo.getString("title");
+                mBookList.add(new Book(title, formatAuthors(authors)));
+            }
+        }
+        catch (JSONException e)
+        {
+            Toast.makeText(this, "Error parsing response", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private String formatAuthors(JSONArray aAuthors)
+    {
+        if (aAuthors.length() == 0)
+        {
+            return "";
+        }
+
+        try {
+            String Result = aAuthors.getString(0);
+
+            for (int i = 1; i < aAuthors.length(); i++) {
+                Result += ", ";
+                Result += aAuthors.getString(i);
+            }
+
+            return Result;
+        }
+        catch (Exception e)
+        {
+            return "";
+        }
     }
 }
