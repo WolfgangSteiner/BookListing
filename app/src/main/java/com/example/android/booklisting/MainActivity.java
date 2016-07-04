e2f7d7bb074395ca817c250e252641f061ed7f0b
Wolfgang Steiner <wolfgang.steiner@gmail.com>
2016-07-04 14:55:53 +0200

    basic program logic working

--- app/src/main/java/com/example/android/booklisting/MainActivity.java
+++ app/src/main/java/com/example/android/booklisting/MainActivity.java
@@ -2,12 +2,32 @@ package com.example.android.booklisting;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity
+{
+    ArrayList<Book> mBookList;
+    BookAdapter mBookAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mBookList = new ArrayList<Book>();
+
+        ListView listView = (ListView) findViewById(R.id.list_view);
+        mBookAdapter = new BookAdapter(this, mBookList);
+        listView.setAdapter(mBookAdapter);
+    }
+
+    public void onStartSearch(View aView)
+    {
+        mBookList.clear();
+        mBookList.add(new Book("20000 miles under the sea", "Jules Verne"));
+        mBookAdapter.notifyDataSetChanged();
     }
 }
