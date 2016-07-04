5c44c7500916d06f792bd288099c3aa23a517fcb
Wolfgang Steiner <wolfgang.steiner@gmail.com>
2016-07-04 15:03:52 +0200

    checking network status

--- app/src/main/java/com/example/android/booklisting/MainActivity.java
+++ app/src/main/java/com/example/android/booklisting/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.android.booklisting;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -26,8 +30,18 @@ public class MainActivity extends AppCompatActivity
 
     public void onStartSearch(View aView)
     {
-        mBookList.clear();
-        mBookList.add(new Book("20000 miles under the sea", "Jules Verne"));
-        mBookAdapter.notifyDataSetChanged();
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+        if (networkInfo != null && networkInfo.isConnected())
+        {
+            mBookList.clear();
+            mBookList.add(new Book("20000 miles under the sea", "Jules Verne"));
+            mBookAdapter.notifyDataSetChanged();
+        }
+        else
+        {
+            Toast.makeText(this, R.string.network_not_reachable, Toast.LENGTH_SHORT).show();
+        }
     }
 }
